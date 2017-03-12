ede071b4d677762d4442aa05abcb163dc6788979
Nusszer Patrik <patriknusszer@20014c4c1aa1b600beaec5fffe717b23.catv.pool.telekom.hu>
2017-03-12 22:26:01 +0100

    Initial Commit

---
+++ Pixie/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Pixie
+//
+//  Created by Patrik Nusszer on 12/03/17.
+//  Copyright © 2017 Patrik Nusszer. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Pixie/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Pixie
+//
+//  Created by Patrik Nusszer on 12/03/17.
+//  Copyright © 2017 Patrik Nusszer. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
